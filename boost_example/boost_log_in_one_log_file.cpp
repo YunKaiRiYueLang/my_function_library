0e53888e5b0378bc0dc027489d8db8ca952e59e2
Sttfsttr <1510627880@qq.com>
2024-07-09 15:58:54 +0800

    Update boost_log_in_one_log_file.cpp
    add demo

--- boost_example/boost_log_in_one_log_file.cpp
+++ boost_example/boost_log_in_one_log_file.cpp
@@ -100,10 +100,45 @@ int main5() {
 	t2.join();
 	return 0;
 }
+int main6() {
+	//指定文件名，日志等级
+	boost::log::add_file_log(boost::log::keywords::file_name = "example.log",
+		boost::log::keywords::open_mode = std::ios_base::app);
+	// 设置全局过滤器，仅允许warning及以上级别的日志  
+	boost::log::core::get()->set_filter
+	(
+		boost::log::trivial::severity >= boost::log::trivial::warning
+	);
+	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+	BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+	BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+	BOOST_LOG_TRIVIAL(error) << "An error severity message";
+	BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
 
 
+	return 0;
+}
+int main7() {
+	boost::log::add_file_log(boost::log::keywords::file_name = "sample_%4N.log",//设置一个可变化的文件名。也可以时  sample_%4N.log    sample_log%Y%m%d_%4N.log.设置成死的文件名。日志无法生成新文件。
+		boost::log::keywords::rotation_size = 1024*100, //0.5kb
+		//达到1024*100字节时，设置滚动文件为example.log.1
+		boost::log::keywords::open_mode = std::ios_base::app);
+	// 设置全局过滤器，仅允许warning及以上级别的日志
+	boost::log::core::get()->set_filter
+	(
+		boost::log::trivial::severity >= boost::log::trivial::warning
+	);
+
+	int i = 0;
+	while (i < 10000) {
+		BOOST_LOG_TRIVIAL(error) << "main5_thread1  An error severity message";
+		i++;
+	}
 
 
+	return 0;
+}
 int main() {
 
 	//未设计复杂的用法。
@@ -111,7 +146,9 @@ int main() {
 	//main2();//日志存入文件中。
 	//main3();//存入指定登记的日志
 	 //main4();//在不同函数中向同一个文件记入日志。
-	main5();//在不同线程中向同一个文件记入日志。
+	//main5();//在不同线程中向同一个文件记入日志。
+	//main6();//每次重新记录日志时，以追加的方式记录。（前面的记录日志的方式，没清空文件内容，从头写入）
+	main7();//日志滚动记录，写到指定大小，再新的文件中写。
 }
 
 
